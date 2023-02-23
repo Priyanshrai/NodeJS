d9da1a4fd8e1f7bc995ed3890a0a38bd4f45d354
Priyanshrai <rai.priyansh007@gmail.com>
2023-02-23 18:03:57 +0530

    Request / Response

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const http = require('http');
 
 const server=http.createServer((req,res)=>{
-console.log('Priyansh rai');
+console.log(req.url);
+if (req.url === '/home') {
+    res.setHeader('Content-Type' ,'text/plain');
+    res.write('Welcome home');
+    res.end();
+  }
+ else if (req.url === '/about') {
+    res.setHeader('Content-Type' ,'text/plain');
+    res.write('Welcome to About Us page');
+    res.end();
+  }
+ else if (req.url === '/node') {
+    res.setHeader('Content-Type' ,'text/plain');
+    res.write('Welcome to my Node Js project');
+    res.end();
+  }
+else{
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>My First Page</title><head>');
+res.write('<body><h1>Hello From My First Node.js Server!<h1></body>');
+res.write('</html>');
+res.end();
+}
+
 });
 
-server.listen(4000);
+server.listen(3000);
